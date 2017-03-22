851f603ba9daf301960d1dc29c312faf5f3b4918
Tom Lous <tomlous@gmail.com>
2017-03-22 12:52:17 +0100

    10/10 score

--- src/main/scala/stackoverflow/StackOverflow.scala
+++ src/main/scala/stackoverflow/StackOverflow.scala
@@ -80,13 +80,13 @@ class StackOverflow extends Serializable {
 
   /** Group the questions and answers together */
   def groupedPostings(postings: RDD[Posting]): RDD[(Int, Iterable[(Posting, Posting)])] = {
-    val postingRead = postings.persist // Read only once from disk
+//    val postingRead = postings.persist // Read only once from disk // Don't cache! => OOM 0/10 score
 
-    val answers = postingRead
+    val answers = postings
       .filter(a => a.postingType == 2 && a.parentId.isDefined)
       .map(a => (a.parentId.get, a))
 
-    val questions = postingRead
+    val questions = postings
       .filter(_.postingType == 1)
       .map(q => (q.id, q))
 
